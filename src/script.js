9aaf2bb77eeb5dbaf97ffecd56d894e28430662e
John Zhang <john.zhang@mountain-pass.com.au>
2022-06-27 17:09:37 +1000

    wip

--- src/script.js
+++ src/script.js
@@ -15,6 +15,33 @@ const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+// Position
+// mesh.position.x = 0.7
+// mesh.position.y = -0.6
+// mesh.position.z = 1
+mesh.position.set(0.7, -0.6, 1)
+
+// distance between the center of the scene and object position
+console.log(mesh.position.length())
+
+// normalization, reduce the vector to 1
+// mesh.position.normalize()
+
+// Scale
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+mesh.scale.set(2, 0.5, 0.5)
+
+// Rotation & Quaternion
+
+
+/**
+ * Axes Helper
+ */
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper)
+
 /**
  * Sizes
  */
@@ -27,8 +54,12 @@ const sizes = {
  * Camera
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
+// camera.position.z = 3
+camera.position.set(1, 1, 3)
 scene.add(camera)
+
+// distance from the mesh to camera
+console.log(mesh.position.distanceTo(camera.position))
 
 /**
  * Renderer
