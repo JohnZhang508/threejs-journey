94ee7525aac084446714eb61212551b225e011b5
JohnZhang508 <john.zhang@mountain-pass.com.au>
2022-07-04 22:41:55 +1000

    complete 07-camera

--- src/script.js
+++ src/script.js
@@ -1,21 +1,12 @@
-import gsap from 'gsap'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import './style.css'
 
-// Clock
-const clock = new THREE.Clock()
-
+/**
+ * Base
+ */
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
-
-// Scene
-const scene = new THREE.Scene()
-
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
 
 // Sizes
 const sizes = {
@@ -23,10 +14,31 @@ const sizes = {
     height: 600
 }
 
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+scene.add(mesh)
+
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+// const aspectRatio = sizes.width / sizes.height
+// const camera = new THREE.OrthographicCamera(-1 * aspectRatio, 1 * aspectRatio, 1, -1, 0.1, 100)
+camera.position.x = 2
+camera.position.y = 2
 camera.position.z = 3
+camera.lookAt(mesh.position)
 scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+// controls.target.y = 2
+// controls.update()
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
@@ -34,25 +46,42 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 
-gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 })
-gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 })
+/**
+ * Cursor
+ */
+const cursor = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = - (event.clientY / sizes.height - 0.5)
+})
 
-// Animation
-const animate = () => {
-    // time
-    const deltaTime = clock.getDelta() // not recommended!
+// Animate
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    // mesh.rotation.x += 0.5 * deltaTime
-    // mesh.position.y = Math.sin(elapsedTime)
-    // camera.position.x = Math.cos(elapsedTime)
-    // camera.position.y = Math.sin(elapsedTime)
+    // mesh.rotation.y = elapsedTime;
+
+    // Update camera
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 5
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 5
+    // camera.position.y = cursor.y * 3
     // camera.lookAt(mesh.position)
+
+    // Update controls in each frame
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
-    requestAnimationFrame(animate)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
 }
 
-animate()
+tick()
